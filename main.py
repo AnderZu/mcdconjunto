12d1d11d5acec76a2bc7eea348b1ab8ff9f6e83c
labf602 <72813012@contienental.edu.pe>
2023-01-31 10:09:36 -0500

    MCD ingreso de datos

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 
+# escribir el codigo de main para ingresar n numero naturales y calcular em MCD de los numeros ingresados
 if __name__ == '__main__':
-    pass
+from src.logica.OperacionesEnteros import OperacionesEnteros
 
+    cantidadNumero = int(input("ingrese cantidad de numeros: "))
+    numero = []
+    for x in range(0, cantidadNumero):
+        numero.append(int(input("ingrese valor: ")))
+    print(numero)
+    resltadoMCD = self.MCDMasDosNumeros()
+print(f"el MCD de {cantidadNumero} numeros los cuales son {numero} es ")
 
