cc011be7f7ab942ff875535837b165dda929a4f6
labf602 <72813012@contienental.edu.pe>
2023-01-31 08:30:43 -0500

    MCD 2 numeros (verde)

--- src/logica/OperacionesEnteras.py
+++ src/logica/OperacionesEnteras.py
@@ -5,4 +5,16 @@ class OperacionesEnteros:
         self.__numeros = numeros
 
     def calcularMCD(self):
+        if len(self.__numeros) == 2:
+            return self.MCD(self.__numeros[0], self.__numeros[1])
+        else:
+            return 0
         return 0
+
+    def MCD(self, numero1, numero2):
+        temporal = 0
+        while numero2 != 0:
+            temporal = numero2
+            numero2 = numero1 % numero2
+            numero1 = temporal
+        return numero1
